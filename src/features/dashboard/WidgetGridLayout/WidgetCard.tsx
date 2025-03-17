541da06a7a502a971daa88e63113b558031c4fe5
Nick <chidojiro@gmail.com>
2025-03-17 23:09:21 +0700

    fix: misaligned responsive widget card

--- src/features/dashboard/WidgetGridLayout/WidgetCard.tsx
+++ src/features/dashboard/WidgetGridLayout/WidgetCard.tsx
@@ -16,10 +16,12 @@ export const WidgetCard = ({ widget, className, ...restProps }: Props) => {
         className={classNames('relative', className)}
         {...restProps}
       >
-        <h3 className='text-xl md:text-2xl font-medium text-center'>{widget.title}</h3>
-        <p className='text-theme-gray-500 text-center line-clamp-2'>{widget.description}</p>
-        <div className='flex flex-col items-center justify-center mt-4'>
-          <p className='text-theme-gray-500 flex items-baseline gap-1 text-xl'>
+        <h3 className='text-xl md:text-2xl font-medium text-center leading-6'>{widget.title}</h3>
+        <p className='text-theme-gray-500 text-center line-clamp-2 leading-5 mt-2'>
+          {widget.description}
+        </p>
+        <div className='flex flex-col items-center justify-center mt-2'>
+          <p className='text-theme-gray-500 flex items-baseline gap-1 text-lg'>
             <b>Preserve ratio:</b>
             {widget.preserveAspectRatio ? (
               <Check className='text-green-500 translate-y-0.5' size={16} />
